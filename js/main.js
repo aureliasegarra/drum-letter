58299ab2dd2e6521a2a640e9ec9d6d568f04508c
aurelia Segarra <av.segarra@gmail.com>
2020-11-09 15:46:50 +0100

    writing function ok

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 
-//Fonction associate a key pressed on the keyboard with a sound
-window.addEventListener('keydown', function(KeyboardEvent){
+
+function playSound(KeyboardEvent){
     const audio = document.querySelector(`audio[data-key="${KeyboardEvent.code}"]`);
     if (!audio) return; // stop the function if no sound associated
     const key = document.querySelector(`.key[data-key="${KeyboardEvent.code}"]`);
     audio.currentTime = 0; // Rewind at the start
     audio.play();
-    console.log(key);
-});
+    key.classList.add('playing');
+};
+
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; // skip it if it's not a transform property
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
